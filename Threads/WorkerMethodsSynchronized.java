7fad67bc4ac5866a645a08147f9b651e8d91e581
unknown <santoshtechpro@gmail.com>
2021-02-26 15:23:52 +0530

    Workerthreadcheck-in after the chnaging the synchronized block

---
+++ Threads/WorkerMethodsSynchronized.java
@@ -0,0 +1,92 @@
+package com.company.thread;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+public class WorkerMethodsSynchronized {
+
+    private Random random = new Random();
+
+    private List<Integer> list1 = new ArrayList<>();
+    private List<Integer> list2 = new ArrayList<>();
+
+    /**
+     * synchronized, methods use different data (list1 list2) so by synchronized
+     * methods if one thread runs the stageOne other thread cannot run stageTwo
+     * at the same time because that same locks are used. Solution is using two
+     * lock Object for two shared data.
+     */
+    public  void stageOne() {
+
+        synchronized (list1) {
+            try {
+                //do your work here
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            list1.add(random.nextInt(100));
+        }
+    }
+
+    public synchronized void stageTwo() {
+
+        synchronized (list2) {
+            try {
+                //do your work here
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            list2.add(random.nextInt(100));
+        }
+    }
+
+    public void process() {
+        for (int i = 0; i < 1000; i++) {
+            stageOne();
+            stageTwo();
+        }
+    }
+
+    public void main() {
+        System.out.println("Starting ...");
+        long start = System.currentTimeMillis();
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                process();
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            public void run() {
+                process();
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException ignored) {}
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("Time taken: " + (end - start));
+        System.out.println("List1: " + list1.size() + "; List2: " + list2.size());
+    }
+}
+
+ class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Synchronized Methods: ");
+        WorkerMethodsSynchronized worker2 = new WorkerMethodsSynchronized();
+        worker2.main();
+    }
+}
